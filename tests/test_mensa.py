c7c429dde8cd6ca9b3a71102eb32d5ee6d7e2a55
Vincent von Hof <vincent.von.hof@wi.uni-muenster.de>
2017-08-07 10:38:49 +0200

    Oh hello there.

--- tests/test_mensa.py
+++ tests/test_mensa.py
@@ -9,9 +9,9 @@ class TestSlack(testtools.TestCase):
     def setUp(self):
         super(TestSlack, self).setUp()
         self.requests_mock = self.useFixture(fixture.Fixture())
-        self.requests_mock.register_uri('GET', mensa.leo_uri)
+        self.requests_mock.register_uri('GET', mensa.mensa_uri)
 
     def testMensaTimeout(self):
-        self.requests_mock.register_uri('GET', mensa.leo_uri, exc=requests.exceptions.Timeout)
+        self.requests_mock.register_uri('GET', mensa.mensa_uri, exc=requests.exceptions.Timeout)
         response = mensa.getMenues()
         assert response is None
