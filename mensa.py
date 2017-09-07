2918d32b5b3cc5b98689f7823b37c397cf3883e0
Vincent von Hof <vincent.von.hof@wi.uni-muenster.de>
2017-09-07 11:48:10 +0200

    Catch-all exception on XML parsing.

--- mensa.py
+++ mensa.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 import requests
 import datetime
 from xml.etree import ElementTree
@@ -30,37 +31,40 @@ def getMenues():
     logging.debug(response.content)
     menueMessage = []
 
-    for menuePerDate in menue:
-        unixtimestamp = float(menuePerDate.attrib.get('timestamp'))
-        lunchDate = datetime.datetime.fromtimestamp(unixtimestamp).date()
-        if lunchDate == datetime.datetime.now().date():
-            dishes = list(menuePerDate.iter('item'))
-            for dish in dishes:
-                meal = dish.find('meal').text
-                mealIcon = dish.find('foodicons')
-                mealCategory = dish.find('category').text
-                mealDescription = meal + ' *Tagesaktion*' if mealCategory == 'Tagesaktion' else meal
+    try:
+        for menuePerDate in menue:
+            unixtimestamp = float(menuePerDate.attrib.get('timestamp'))
+            lunchDate = datetime.datetime.fromtimestamp(unixtimestamp).date()
+            if lunchDate == datetime.datetime.now().date():
+                dishes = list(menuePerDate.iter('item'))
+                for dish in dishes:
+                    meal = dish.find('meal').text
+                    mealIcon = dish.find('foodicons')
+                    mealCategory = dish.find('category').text
+                    mealDescription = meal + ' *Tagesaktion*' if mealCategory == 'Tagesaktion' else meal
 
-                # Python EAFP concept
-                icons = []
-                if mealIcon is not None and mealIcon.text is not None:
-                    ingredients = mealIcon.text.split(',')
-                    for i in ingredients:
-                        try:
-                            icons.append(foodIcons[str.lower(i.strip())])
-                        except KeyError as keyErr:
-                            logging.error('We have no mapping in our foodIcons for the ' + i.strip() + ' icon: '
-                                          + keyErr.message)
-                iconText = None
-                if len(icons) == 0:
-                    if mealCategory == 'Dessertbuffet':
-                        iconText = ':ice_cream: '
+                    # Python EAFP concept
+                    icons = []
+                    if mealIcon is not None and mealIcon.text is not None:
+                        ingredients = mealIcon.text.split(',')
+                        for i in ingredients:
+                            try:
+                                icons.append(foodIcons[str.lower(i.strip())])
+                            except KeyError as keyErr:
+                                logging.error('We have no mapping in our foodIcons for the ' + i.strip() + ' icon: '
+                                              + keyErr.message)
+                    iconText = None
+                    if len(icons) == 0:
+                        if mealCategory == 'Dessertbuffet':
+                            iconText = ':ice_cream: '
+                        else:
+                            iconText = ':question: '
                     else:
-                        iconText = ':question: '
-                else:
-                    iconText = ''.join(icons)
-                mealDescription = iconText + mealDescription
+                        iconText = ''.join(icons)
+                    mealDescription = iconText + mealDescription
 
-                menueMessage.append(mealDescription)
-
+                    menueMessage.append(mealDescription)
+    except Exception:
+        logging.error(traceback.format_exc())
+        # Logs the error appropriately.
     return menueMessage
